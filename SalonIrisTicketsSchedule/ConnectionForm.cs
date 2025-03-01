2861e949ecc3ba5e48567a7bd8c82fe6782fa51e
aqudei <fdeu.aalmine@gmail.com>
2025-03-01 16:43:20 +0800

    mmm

--- SalonIrisTicketsSchedule/ConnectionForm.cs
+++ SalonIrisTicketsSchedule/ConnectionForm.cs
@@ -359,8 +359,8 @@ namespace SalonIrisTicketsSchedule
 
         private void RefreshScreen()
         {
-            // var now = DateTime.Now;
-            var now = DateTime.Parse("2025-03-01 11:30");
+            var now = DateTime.Now;
+            // var now = DateTime.Parse("2025-03-01 11:30");
 
             var tickets = GetTickets(now.Date);
             var schedules = GetSchedules(now.Date);
